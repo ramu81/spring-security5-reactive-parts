b11196c748cd252170381b73bffabcd476ec75a6
Ramu <ramudu.kp@gmail.com>
2017-12-12 16:14:43 -0500

    logging

--- src/main/java/sample/IndexController.java
+++ src/main/java/sample/IndexController.java
@@ -1,5 +1,9 @@
 package sample;
 
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,8 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class IndexController {
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 	@GetMapping("/")
 	String index() {
+		Stream.of(Thread.currentThread().getStackTrace()).forEach(i -> {
+			logger.info("index() ==> " + i.toString());
+		});
 		return "redirect:/inbox";
 	}
 }
