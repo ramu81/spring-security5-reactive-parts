841767f20e1caa3aa79c17ff0202d9724ffdba63
Ramu <ramudu.kp@gmail.com>
2017-12-11 23:19:06 -0500

    # WARNING: head commit changed in the meantime
    update files

--- src/main/java/sample/message/MessageController.java
+++ src/main/java/sample/message/MessageController.java
@@ -1,20 +1,23 @@
 package sample.message;
 
-import reactor.core.publisher.Mono;
-import sample.security.CurrentUser;
-import sample.user.User;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.result.view.Rendering;
 
+import reactor.core.publisher.Mono;
+import sample.security.CurrentUser;
+import sample.user.User;
+
 /**
  * @author Rob Winch
  */
 @Controller
 public class MessageController {
+	private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
 	private final MessageRepository messages;
 
 	public MessageController(MessageRepository messages) {
@@ -23,6 +26,7 @@ public class MessageController {
 
 	@GetMapping("/inbox")
 	Rendering inbox(@CurrentUser User user) {
+		logger.info(" user  "+user.getId());
 		return Rendering.view("messages/inbox")
 				.modelAttribute("messages", this.messages.findByTo(user.getId()))
 				.build();
@@ -31,6 +35,7 @@ public class MessageController {
 
 	@GetMapping("/messages/{id}")
 	Rendering message(@PathVariable Long id) {
+		logger.info(" message id  "+id);
 		return Rendering.view("messages/view")
 				.modelAttribute("message", this.messages.findById(id))
 				.build();
