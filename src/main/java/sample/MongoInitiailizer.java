841711f76a46dc74c4a5b8cf3e6bcd8760c40554
Ramu <ramudu.kp@gmail.com>
2017-12-11 22:19:05 -0500

    update

--- src/main/java/sample/MongoInitiailizer.java
+++ src/main/java/sample/MongoInitiailizer.java
@@ -1,9 +1,12 @@
 package sample;
 
 import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import sample.message.Message;
 import sample.message.MessageRepository;
+import sample.sequence.NextSequenceService;
 import sample.user.User;
 import sample.user.UserRepository;
 
@@ -14,6 +17,8 @@ import sample.user.UserRepository;
 class MongoInitiailizer implements SmartInitializingSingleton {
 	private final MessageRepository messages;
 	private final UserRepository users;
+	@Autowired
+	private NextSequenceService service;
 
 	MongoInitiailizer(MessageRepository messages, UserRepository users) {
 		this.messages = messages;
@@ -24,25 +29,22 @@ class MongoInitiailizer implements SmartInitializingSingleton {
 	public void afterSingletonsInstantiated() {
 		// sha256 w/ salt encoded "password"
 		String passsword = "73ac8218b92f7494366bf3a03c0c2ee2095d0c03a29cb34c95da327c7aa17173248af74d46ba2d4c";
-
-		User rob = new User(1L, "rob@example.com", passsword, "Rob", "Winch");
-		User joe = new User(100L, "joe@example.com", passsword, "Joe", "Grandja");
-		User vedran = new User(200L, "vedran@example.com", passsword, "Vedran", "Pavić");
+		User rob = new User(service.getNextSequence("customSequences"), "rob@example.com", passsword, "Rob", "Winch");
+		User joe = new User(service.getNextSequence("customSequences"), "joe@example.com",
+				passsword, "Joe", "Grandja");
+		User vedran = new User(service.getNextSequence("customSequences"), "vedran@example.com", passsword, "Vedran", "Pavić");
 
 		this.users.save(rob).block();
 		this.users.save(joe).block();
 		this.users.save(vedran).block();
 
 		this.messages.save(new Message(1L, rob, joe, "Hello World")).block();
-		this.messages.save(new Message(2L, rob, joe,"Greetings SpringOne")).block();
-		this.messages.save(new Message(3L, rob, vedran, "Hi San Francisco")).block();
+		this.messages.save(new Message(2L, rob, joe, "Greetings SpringOne")).block();
+		this.messages.save(new Message(3L, joe, vedran, "Hi San Francisco")).block();
 
 		// @formatter:off
-		this.users.findAll()
-				.doOnNext(user -> user.setPassword("{sha256}" + user.getPassword()))
-				.flatMap(this.users::save)
-				.collectList()
-				.block();
+		this.users.findAll().doOnNext(user -> user.setPassword("{sha256}" + user.getPassword()))
+				.flatMap(this.users::save).collectList().block();
 		// @formatter:on
 
 	}
